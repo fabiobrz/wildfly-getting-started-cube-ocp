cd3863912827646adac0a32df97fb28b3e25e714
Fabio Burzigotti <fburzigo@redhat.com>
2025-06-11 10:19:07 +0200

    Make things simple, for the example to work just for OpenShift (no profiles etc.)

--- src/test/java/org/wildfly/examples/GettingStartedOpenShiftApplicationIT.java
+++ src/test/java/org/wildfly/examples/GettingStartedOpenShiftApplicationIT.java
@@ -5,8 +5,9 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.Response;
 import org.apache.http.HttpStatus;
 import org.arquillian.cube.openshift.impl.enricher.RouteURL;
-import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.awaitility.Awaitility;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,9 +15,7 @@ import java.net.URI;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@RunWith(ArquillianConditionalRunner.class)
+@RunWith(Arquillian.class)
 public class GettingStartedOpenShiftApplicationIT {
 
     @RouteURL("hello-world-svc")
@@ -24,25 +23,29 @@ public class GettingStartedOpenShiftApplicationIT {
 
     @Test
     public void helloEndpointShouldReplyWithHttp200() {
-        // The OpenShift Route resource for the application to be available outside the cluster will take some time to be ready
-        Awaitility.await()
-                .atMost(60, TimeUnit.SECONDS)
-                .until( () -> {
-                    Response statusResponse = RestAssured.given()
-                            .when()
-                            .get(serviceUrl);
-                    return statusResponse.statusCode() == HttpStatus.SC_OK;
-                } );
+        final String serviceUrl = url.toString();
 
         try (Client client = ClientBuilder.newClient()) {
-            jakarta.ws.rs.core.Response response = client
-                    .target(URI.create(url.toString()))
+            // The OpenShift Route resource for the application to be available outside the cluster will take some time to be ready
+            Awaitility.await()
+                    .atMost(60, TimeUnit.SECONDS)
+                    .until( () -> {
+                        Response statusResponse = client
+                                .target(URI.create(serviceUrl))
+                                .path("/hello/World")
+                                .request()
+                                .get();
+                        return statusResponse.getStatus() == HttpStatus.SC_OK;
+                    } );
+
+            Response response = client
+                    .target(URI.create(serviceUrl))
                     .path("/hello/World")
                     .request()
                     .get();
 
-            assertEquals(200, response.getStatus());
-            assertEquals("Hello 'World'.", response.readEntity(String.class));
+            Assert.assertEquals(200, response.getStatus());
+            Assert.assertEquals("Hello 'World'.", response.readEntity(String.class));
 
         }
     }
